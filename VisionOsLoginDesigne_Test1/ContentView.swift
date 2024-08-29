f4d707cd604028f9e4c9599ddd5d8378adcbc3f7
Famil Mustafayev <dijital_human@icloud.com>
2024-08-29 05:42:55 +0400

    Initial Commit

---
+++ VisionOsLoginDesigne_Test1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionOsLoginDesigne_Test1
+//
+//  Created by Famil Mustafayev on 29.08.24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionOsLoginDesigne_Test1/VisionOsLoginDesigne_Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOsLoginDesigne_Test1App.swift
+//  VisionOsLoginDesigne_Test1
+//
+//  Created by Famil Mustafayev on 29.08.24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOsLoginDesigne_Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
