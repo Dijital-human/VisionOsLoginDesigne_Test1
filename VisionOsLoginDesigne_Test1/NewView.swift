41eccd257f0ffbe2e868a6099c917296ba8c680d
Famil Mustafayev <dijital_human@icloud.com>
2024-08-29 06:26:32 +0400

    New View Added Text

---
+++ VisionOsLoginDesigne_Test1/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  VisionOsLoginDesigne_Test1
+//
+//  Created by Famil Mustafayev on 29.08.24.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
